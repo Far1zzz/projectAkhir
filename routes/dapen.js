f7b420f2bfc4bc5a4f196dcc2db9830ceb75d3f9
farizz <fairizwalker@gmail.com>
2022-12-02 00:39:36 +0700

    update get put delete

--- routes/dapen.js
+++ routes/dapen.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const DataPenduduk = require("../models/Dapen");
 
-// create
+// buat
 router.post("/", async (req, res) => {
   // menampungan input
   const dapenPost = new DataPenduduk({
@@ -15,6 +15,51 @@ router.post("/", async (req, res) => {
     const dapen = await dapenPost.save();
 
     // response
+    res.json(dapen);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Get data
+router.get("/", async (req, res) => {
+  try {
+    const dapen = await DataPenduduk.find();
+    res.json(dapen);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+//  put data
+
+router.put("/:pendudukId", async (req, res) => {
+  // tampungan input data penduduk
+  const data = {
+    nama: req.body.nama,
+    alamat: req.body.alamat,
+  };
+
+  try {
+    // update data
+    const dapen = await DataPenduduk.updateOne(
+      { _id: req.params.pendudukId },
+      data
+    );
+    res.json(dapen);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// delete data
+router.delete("/:pendudukId", async (req, res) => {
+  try {
+    // delete
+    const dapen = await DataPenduduk.deleteOne({
+      _id: req.params.pendudukId,
+    });
+    // res
     res.json(dapen);
   } catch (error) {
     res.json({ message: error });
